84b27cbb29b502e3dc8237d006250c688b6d9c54
VaderSteve76 <alverson.s@yahoo.com>
2020-05-05 16:21:18 -0400

    Added functions for previous song and next song

--- script.js
+++ script.js
@@ -40,6 +40,26 @@ function pauseSong() {
   audio.pause();
 };
 
+// Previous song
+function prevSong() {
+  songIndex--;
+  if(songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+// Next song
+function nextSong() {
+  songIndex++;
+  if(songIndex > songs.length -1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
 
 // Event Listeners
 playBtn.addEventListener('click', () => {
